3c08f15e83121a35dc935aa9aad215c8ccf78bf3
Miles Lawrence Basnillo <mbasnillo@isbx.com>
2018-04-19 15:04:45 +0800

    Initial Commit

---
+++ Swift Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Calculator
+//
+//  Created by Miles R. Basnillo on 19/04/2018.
+//  Copyright © 2018 Miles R. Basnillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
